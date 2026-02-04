bfbab04e96938e147ec29984b4efb4a69ebd4f3a
Hashir V <82193429+hashirventhodi@users.noreply.github.com>
2026-02-04 11:06:19 +0000

    chore: release v0.4.0

--- web/src/constants/VERSION.ts
+++ web/src/constants/VERSION.ts
@@ -4,4 +4,4 @@
  * This version is automatically updated during the release process.
  * Displayed in the UI footer and about page.
  */
-export const VERSION = "v0.3.0";
+export const VERSION = "v0.4.0";
